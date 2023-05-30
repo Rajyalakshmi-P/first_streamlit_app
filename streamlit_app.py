55b3a81b832fd44887157d604ca1cf00d74fe595
Rajyalakshmi-P <135049307+Rajyalakshmi-P@users.noreply.github.com>
2023-05-30 11:58:43 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Spinach sandwich')
+streamlit.text('Hard-Boiled Free-Range egg')
